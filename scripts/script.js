2f3590539f6a0643ac6b78404bdf1b88e648fd25
Jess Dowd <jessalyndowd@gmail.com>
2021-01-08 04:06:14 -0600

    fix:layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,32 @@
- 
+
 let openBox = document.querySelector('.profile__text-button');
 let closeBox = document.querySelector(".popup-box__exit");
 let popupBox = document.querySelector(".popup-box");
-let formElement =  document.querySelector(".popup-box__save");
+let formElement = document.querySelector(".popup-box__save");
 let nameInput = document.querySelector(".popup-box__text_name");
 let jobInput = document.querySelector(".popup-box__text_about");
 let currentName = document.querySelector(".profile__name");
 let currentBio = document.querySelector(".profile__bio");
 
 function open() {
-  popupBox.classList.add ("popup-box_open");
+  popupBox.classList.add("popup-box_open");
   nameInput.value = currentName.textContent
   jobInput.value = currentBio.textContent
-  
+
 }
 
 function close() {
   popupBox.classList.remove("popup-box_open");
-  
+
 }
 
 
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
 
   currentName.textContent = nameInput.value;
   currentBio.textContent = jobInput.value;
-  close()   
+  close()
 };
 
 
